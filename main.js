2fcec48005377abb7788d5968f37b02b73aa3465
Abeku <acobbina717@gmail.com>
2022-04-13 00:53:32 -0400

    commit tree 7aee0a957e418165450802b81e5278fb8e5ae767

--- main.js
+++ main.js
@@ -4,6 +4,31 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likes = document.querySelectorAll(".like-glyph")
+likes.forEach((like) => {
+  like.addEventListener("click", heartOrNoHeart)
+})
+
+function heartOrNoHeart(e) {
+  const heart = e.target;
+  mimicServerCall().then(() => {
+    if (heart.innerText === EMPTY_HEART) {
+      heart.innerText = FULL_HEART;
+      heart.className = "activated-heart"
+    } else if (heart.innerText === FULL_HEART) {
+      heart.innerText = EMPTY_HEART;
+      heart.className = ""
+    }
+  })
+    .catch((error) => {
+      const modal = document.querySelector("#modal")
+      modal.className = "";
+      const message = document.querySelector("#modal-message")
+      message.innerText = error;
+      setTimeout((() => { modal.className = "hidden" }, 3000))
+    })
+}
+
 
 
 
@@ -11,9 +36,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
